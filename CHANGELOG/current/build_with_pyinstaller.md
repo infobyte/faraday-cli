285e09b86a561fd0823df960f9d83811b8a38ce7
Nicolas Rebagliati <nrebagliati@infobytesec.com>
2020-12-21 18:37:44 -0300

    build with pyinstaller

---
+++ CHANGELOG/current/build_with_pyinstaller.md
@@ -0,0 +1,1 @@
+Add support for build with pyinstaller
---
+++ launcher.py
@@ -0,0 +1,5 @@
+# this launcher is generated for pyinstaller
+from faraday_cli.shell.main import main
+
+if __name__ == "__main__":
+    main()
