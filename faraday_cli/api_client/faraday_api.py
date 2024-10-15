17833d2e849975b2297b280f0fa4386950947ab5
Adriano Canavero <acanavero@faradaysec.com>
2024-10-15 14:08:39 -0300

    erase comments

--- faraday_cli/api_client/faraday_api.py
+++ faraday_cli/api_client/faraday_api.py
@@ -282,14 +282,9 @@ class FaradayApi:
 
     @handle_errors
     def get_vulns(self, workspace_name: str, query_filter: dict = None):
-        # if query_filter.get("filters"):
         response = self.faraday_api.vuln.filter(
             workspace_name, params={"q": json.dumps(query_filter)}
         )
-        # else:
-        #     response = self.faraday_api.vuln.list(
-        #         workspace_name, params={"sort": "severity", "sort_dir": "asc"}
-        #     )
         return response.body
 
     @handle_errors
